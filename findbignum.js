be0993acefdecab5c057dcc9b2d38ea0190cc72c
surendherbau1330 <surendherbau1330@gmail.com>
2025-09-30 06:36:38 +0530

    update

---
+++ findbignum.js
@@ -0,0 +1,12 @@
+// normally two numbers
+
+function find(a,b){
+    if(a > b){
+        console.log(`${a} is a ig number`)
+    }else{
+        console.log(`${b} is a big number`)
+    }
+}
+
+find(45,34)
+find(50,23)
---
+++ findlastdigit.js
@@ -0,0 +1,6 @@
+let a = 100;
+let b = 80;
+let c = 30;
+
+ a > b ? (a > c ? console.log(`a is larger ${a}`) : c) : b > c ? b : c ;
+// console.log(ans)
